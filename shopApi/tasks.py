e707e99d9dfea43e70891cf1b5dbeb9f604cb0d0
Syimykurmanov <syimyk.kurmanov@gmail.com>
2023-02-07 20:24:07 +0600

    off coments

--- shopApi/tasks.py
+++ shopApi/tasks.py
@@ -14,14 +14,14 @@ def send_confirm_email_task(user, code):
 @app.task
 def send_notification_task(user_email, order_id, price):
     send_notification(user_email, order_id, price)
-#
-# @app.task
-# def send_spam_email():
-#     for user in Spam_Contacts.objects.all():
-#         send_mail(
-#             'SPAM SPAM SPAM',
-#             'THIS IS SPAM LETTER FOR YOU FROM JOHN SNOW!',
-#             'syimyk.kurmanov@gmail.com',
-#             [user.email],
-#             fail_silently=False,
-#         )
+
+@app.task
+def send_spam_email():
+    for user in Spam_Contacts.objects.all():
+        send_mail(
+            'SPAM SPAM SPAM',
+            'THIS IS SPAM LETTER FOR YOU FROM JOHN SNOW!',
+            'syimyk.kurmanov@gmail.com',
+            [user.email],
+            fail_silently=False,
+        )
